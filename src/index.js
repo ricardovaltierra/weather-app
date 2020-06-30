2d2ededffbd0f763ef395ddb36846534a6fcbae9
Ricardo Valtierra <ricardo_valtierra@outlook.com>
2020-06-30 17:33:30 -0500

    Revert "Api implementation to get location and weather"

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './icons.scss';
-import './backgrounds.scss';
-import './style.scss';
-import { createElements } from './domHandler';
-import getPlace from './apiconnection';
-
-createElements();
-getPlace();
