ce4f142dfa0f2899d990d28fd2a137958f8a2e31
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-08-11 12:43:50 -0500

    added meta tags on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,16 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       title: 'Weather App',
+      filename: 'index.html',
+      meta: {
+        author: 'Ricardo Valtierra',
+        'og:image': { 'property': 'og:image', content: '../src/img/usage_1.gif' },
+        'linkedin:image': { 'name': 'linkedin:image', content: '../src/img/usage_1.gif' },
+        'og:url': { 'property': 'og:url', content: 'https://weather-query.netlify.app/' },
+        'linkedin:card':  'summary',
+        'og:description': { 'property': 'og:description', content: 'Weather App for a global forecast on F° and C°' },
+        'og:title': { 'property': 'og:title', content: 'Weather App' },        
+      },
     }),
   ],
   output: {
